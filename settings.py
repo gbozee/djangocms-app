e2e62b967320e38d177ec925b1182516d69533a6
Beesama <gbozee@gmail.com>
2018-11-20 06:16:07 +0100

    changes to navigation.html

--- settings.py
+++ settings.py
@@ -28,5 +28,55 @@ aldryn_addons.settings.load(locals())
 
 INSTALLED_APPS.extend([
     # add your project specific apps here
+    "haystack",
+    "aldryn_search",
+    "standard_form",
+    "spurl",
+        
     "design",
 ])
+
+HAYSTACK_ROUTERS = ["aldryn_search.router.LanguageRouter"]
+
+ALDRYN_SEARCH_LANGUAGE_FROM_ALIAS = lambda alias: alias.split("-")[-1]
+
+ALDRYN_PEOPLE_SEARCH = False
+ALDRYN_SEARCH_REGISTER_APPHOOK = True
+
+ALDRYN_NEWSBLOG_UPDATE_SEARCH_DATA_ON_SAVE = True
+ALDRYN_SEARCH_DEFAULT_LANGUAGE = "en"
+
+import os
+
+HAYSTACK_CONNECTIONS = {
+    "default": {
+        "ENGINE": "haystack.backends.whoosh_backend.WhooshEngine",
+        "PATH": os.path.join(os.path.dirname(__file__), "whoosh_index"),
+    },
+    "en": {
+        "ENGINE": "haystack.backends.whoosh_backend.WhooshEngine",
+        "PATH": os.path.join(os.path.dirname(__file__), "whoosh_index"),
+    },
+}
+
+
+DJANGOCMS_STYLE_TAGS = [
+    "div",
+    "main",
+    "article",
+    "section",
+    "header",
+    "footer",
+    "aside",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+]
+DJANGOCMS_STYLE_CHOICES = ["container", "content", "bg-col", "row"]
+DJANGOCMS_STYLE_TEMPLATES = [
+    ["default", "Default"],
+    ["section_with_container_row", "Section with Container and Row"],
+]
